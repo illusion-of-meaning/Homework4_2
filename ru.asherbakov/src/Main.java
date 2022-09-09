dfa68457bcf59df8e732230a7c0d5abc38d9e62f
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-09-09 23:00:04 +0900

    Complite

--- ru.asherbakov/src/Main.java
+++ ru.asherbakov/src/Main.java
@@ -25,21 +25,77 @@ public class Main {
                 i--;
                 System.out.print(i + " ");
             }
+            System.out.println();
         }
         // 3
         {
             int totalPeople = 12_000_000;
             int birthrate = (totalPeople / 1000) * 17;
             int mortality = (totalPeople / 1000) * 8;
-            int year = 0;
-            while (year < 10) {
-
+            int year = 1;
+            while (year <= 10) {
+                totalPeople = totalPeople + birthrate - mortality;
+                System.out.println(String.format("Год %s, численность населения составляет %s", year, totalPeople));
+                year++;
             }
         }
         // ДЗ 2
         // 1
         {
+            int sumPayment = 15000;
+            int month = 0;
+            while (sumPayment <= 12_000_000) {
+                month++;
+                sumPayment += sumPayment / 100 * 7;
+                System.out.println(String.format("Месяц %s, сумма накоплений: %s", month, sumPayment));
+            }
+        }
+        // 2
+        {
+            int sumPayment = 15000;
 
+            for (int month = 1; sumPayment < 12_000_000; month++) {
+                sumPayment += sumPayment / 100 * 7;
+                if (month % 6 == 0) {
+                    System.out.println(String.format("Месяц %s, сумма накоплений: %s", month, sumPayment));
+                }
+            }
+        }
+        // 3
+        {
+            int sumPayment = 15000;
+
+            for (int month = 1; month <= 108; month++) {
+                sumPayment += sumPayment / 100 * 7;
+                if (month % 6 == 0) {
+                    System.out.println(String.format("Месяц %s, сумма накоплений: %s", month, sumPayment));
+                }
+            }
+        }
+        // 4
+        {
+            int day = 3;
+            for (int i = day; i <= 31; i += 7) {
+                System.out.println(String.format("Сегодня пятница, %s-е число. Необходимо подготовить отчет.", i));
+            }
+        }
+        // ДЗ 3
+        // 1
+        {
+            int year = 2022;
+            int yearBefore = year - 200;
+            int yearAfter = year + 100;
+            for (int i = yearBefore; i <= yearAfter; i++) {
+                if (i % 79 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+        // 2
+        {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i + " * 2 = " + i*2);
+            }
         }
     }
 }
